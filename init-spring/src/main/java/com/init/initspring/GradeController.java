05a6801972f9fc75c1b9c7769880fccba3bc0b44
Fahim Al Sami <46305384+M0nster92@users.noreply.github.com>
2022-11-29 04:22:15 +0000

    spring thymeleaf

--- init-spring/src/main/java/com/init/initspring/GradeController.java
+++ init-spring/src/main/java/com/init/initspring/GradeController.java
@@ -1,12 +1,15 @@
 package com.init.initspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
     @GetMapping("/grades")
-    public String getGrades(){
-        return "grades";
+    public String getGrades(Model model){
+        Grade grade = new Grade("Harry", "Arts", "A++");
+        model.addAttribute("grade", grade);
+        return "grades.html";
     }
 }
