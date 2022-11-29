617cb2d105d3dd0f8675b896e4c7aa1efa1153c5
Fahim Al Sami <46305384+M0nster92@users.noreply.github.com>
2022-11-29 01:40:17 +0000

    stack leet codes

---
+++ full/src/leetcode/stack/Ques_150.java
@@ -0,0 +1,25 @@
+package leetcode.stack;
+
+public class Ques_150 {
+    public static void evalRPN(string[] tokens){
+        Stack<Integer> stack = new Stack<>();
+        for(var token : tokens){
+            if(token.equals("+")) stack.push(stack.pop() + stack.pop());
+            else if(token.equals("-")){
+                int a = (int) stack.pop();
+                int b = (int) stack.pop();
+
+                stack.push(b - a);
+            }
+            else if(token.equals("*")) stack.push(stack.pop() * stack.pop());
+            else if(token.equals("/")) {
+                int a = (int) stack.pop();
+                int b = (int) stack.pop();
+
+                stack.push(b / a);
+            }
+            else stack.push(Integer.parseInt(token));
+        }
+        return stack.peek();
+    }
+}
