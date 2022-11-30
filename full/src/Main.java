5eade9b14ad22a54a7b7daaa935c80bfda3b9ffd
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2022-11-29 19:01:43 -0700

    sync

--- full/src/Main.java
+++ full/src/Main.java
@@ -4,11 +4,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.addLast(5);
-        list.addFirst(2);
-        list.removeLast();
-        System.out.println(list.indexOf(2));
+        String s = "Hello";
+        String reversed = "";
+        for(int i = s.length() - 1; i >= 0; i--){
+            reversed += s.charAt(i);
+        }
+        System.out.println(reversed);
+    }
+
+    public static String reverse(String s){
+        Stack<Character> stack = new Stack<>();
+        for(var c : s.toCharArray()){
+            stack.push(c);
+        }
+         String reversed = "";
+        while(!stack.empty()){
+            reversed += stack.pop();
+        }
+
+        return reversed;
     }
 
     public static boolean valid(String s){
