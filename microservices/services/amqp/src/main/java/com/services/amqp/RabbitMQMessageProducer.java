97a926d4e230af4ac160d5df1a8118b486a43828
M0nster92 <fahimalsafi@gmail.com>
2022-12-22 12:23:19 -0700

    added rabbitmq module

---
+++ microservices/services/amqp/src/main/java/com/services/amqp/RabbitMQMessageProducer.java
@@ -0,0 +1,17 @@
+package com.services.amqp;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class RabbitMQMessageProducer {
+    private final AmqpTemplate amqpTemplate;
+
+    public void publish(Object payload, String exchange, String routingKey){
+        amqpTemplate.convertAndSend(exchange, routingKey, payload);
+    }
+}
--- microservices/services/notification/src/main/java/com/services/notification/NotificationApplication.java
+++ microservices/services/notification/src/main/java/com/services/notification/NotificationApplication.java
@@ -1,11 +1,19 @@
 package com.services.notification;
 
+import com.services.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.services.notification",
+                "com.services.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.services.clients"
@@ -14,4 +22,11 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer rabbitMQMessageProducer, NotificationConfig notificationConfig){
+//        return args -> {
+//            rabbitMQMessageProducer.publish("foo", notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
 }
