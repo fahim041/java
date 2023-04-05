f315ac683cf4c8ff1bd2c1f81102b1411f4a4685
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2023-04-04 19:47:08 -0600

    sync

--- full/src/Main.java
+++ full/src/Main.java
@@ -1,21 +1,5 @@
-import design_patterns.behavioural.memento.Editor;
-import design_patterns.behavioural.memento.History;
-
 public class Main {
     public static void main(String[] args) {
-        Editor editor = new Editor();
-        History history = new History();
-
-        editor.setContent("a", "user-1");
-        history.push(editor.createState());
-
-        editor.setContent("b", "user-2");
-        history.push(editor.createState());
-
-        editor.setContent("c", "user-1");
-        editor.restore(history.pop());
-        editor.restore(history.pop());
-
-        System.out.println(editor.getContent() +" "+ editor.getUser());
+        System.out.println("hello");
     }
 }
